fd1129e0acc3ae607a5286bcdd60a9aa3618cf0c
KunalDuran <kunalduran13@gmail.com>
2025-03-27 00:39:49 +0530

    server path

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://192.168.1.8:8080',
+  baseURL: 'https://fotuj.duranz.in',
 });
 
 export const fetchImages = async (status) => {
